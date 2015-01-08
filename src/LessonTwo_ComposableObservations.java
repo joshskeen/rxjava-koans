6f3d332471fd5c4e58483e67267c6fa5c9d53dd5
Jason Atwood <jatwood@bignerdranch.com>
2015-01-08 11:59:43 -0500

    - renames member variables - adds comments to first two tests. - removes filter() element from second test (does filtering with if statement) - renames first two tests

--- src/LessonTwo_ComposableObservations.java
+++ src/LessonTwo_ComposableObservations.java
@@ -7,69 +7,76 @@ import static org.fest.assertions.Assertions.assertThat;
 
 public class LessonTwo_ComposableObservations {
 
-    public int mReceived;
-    public String mValueA;
-    public String mValueB;
+    public int mSum;
+    public String mStringA;
+    public String mStringB;
     public Boolean mBooleanValue;
     private Object _____;
     private int ____;
 
     /*
-    Before when we added up each emitted item, we did the addition ourselves
-    (see LessonOne.everythingCounts() ). Each item that came through the stream
-    was added to a member variable that held onto the sum, one at a time.
+    So far, each of our observables only did one thing; they only had one bit of code inside either subscribe() or doOnNext().
+    However the real power of reactive functional programming comes from that fact that we can combine functions together
+    to do multiple things to our items. We say that we are "composing sequences". There are two ways to do this. Lets look at the first way:
 
-    However, we can also apply an operation to all items emitted "at once". Lets sum all the items
-    emitted by the "numbers" observable.
+    We can pass one observable as an input to another. Here we are going to create an Observable of Integers and then pass
+    it to a second Observable: special MathObservable which will sum the integers.
     */
     @Test
-    public void composableAddition() {
-        mReceived = 0;
-        int ____ = 0;
+    public void composableObservables() {
+        mSum = 0;
         Observable<Integer> numbers = Observable.just(10, 100, ____);
-        MathObservable.sumInteger(numbers).subscribe(integer -> mReceived = integer);
-        assertThat(mReceived).isEqualTo(1110);
+        MathObservable.sumInteger(numbers).subscribe(integer -> mSum = integer);
+        assertThat(mSum).isEqualTo(1110);
     }
 
+    /*
+    The second way we can compose sequences is with a fluent style. We can chain methods together to achieve more functionality.
+    In this example we have one Observable and we "sequentially" perform two actions. Lets build two Strings by concatenating some integers.
+     */
     @Test
-    public void composableBeforeAndAfter() {
-        mValueA = "";
-        mValueB = "";
-        Observable.range(1, 6).doOnNext(integer -> mValueA += integer)
-                .filter(integer -> integer % 2 == 0)
-                .doOnNext(integer -> mValueB += integer)
+    public void composableFunctions() {
+        mStringA = "";
+        mStringB = "";
+        Observable.range(1, 6)
+                .doOnNext(integer -> mStringA += integer)
+                .doOnNext(integer -> {
+                    if (integer % 2 == 0) {
+                        mStringB += integer;
+                    }
+                })
                 .subscribe();
-        assertThat(mValueA).isEqualTo("123456");
-        assertThat(mValueB).isEqualTo("246");
+        assertThat(mStringA).isEqualTo("____");
+        assertThat(mStringB).isEqualTo("____");
     }
 
     @Test
     public void weWroteThis() {
-        mValueA = "";
+        mStringA = "";
         int ____ = 0;
         StringObservable.join(Observable.just("Josh", "Bill", "Joe", "Fredric")
-                .filter(s -> s.length() <= ____), ",").subscribe(s -> mValueA = s);
-        assertThat(mValueA).isEqualTo("Josh,Bill,Joe");
+                .filter(s -> s.length() <= ____), ",").subscribe(s -> mStringA = s);
+        assertThat(mStringA).isEqualTo("Josh,Bill,Joe");
     }
 
     @Test
     public void convertingEvents() {
-        mValueA = "";
-        Observable.just("wE", "hOpe", "yOU", "aRe", "eNjOyInG", "thIS").map(s -> _____).subscribe(s -> mValueA += s + " ");
-        assertThat(mValueA).isEqualTo("we hope you are enjoying this ");
+        mStringA = "";
+        Observable.just("wE", "hOpe", "yOU", "aRe", "eNjOyInG", "thIS").map(s -> _____).subscribe(s -> mStringA += s + " ");
+        assertThat(mStringA).isEqualTo("we hope you are enjoying this ");
     }
 
     @Test
     public void creatingAMoreRelevantEventStream() {
-        mValueA = "";
+        mStringA = "";
         final int windowTopX = 50;
-        Observable.just(100, 200, 150).map(integer -> integer - ____).subscribe(integer -> mValueA += integer + ", ");
-        assertThat(mValueA).isEqualTo("50, 150, 100, ");
+        Observable.just(100, 200, 150).map(integer -> integer - ____).subscribe(integer -> mStringA += integer + ", ");
+        assertThat(mStringA).isEqualTo("50, 150, 100, ");
     }
 
     @Test
     public void checkingEverything() {
-        mReceived = 0;
+        mSum = 0;
         Observable.just(2, 4, 6, 8).all(integer -> integer % 2 == 0).subscribe(aBoolean -> mBooleanValue = aBoolean);
         assertThat(_____).isEqualTo(true);
     }
