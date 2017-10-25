a95e8e2537cf2d49c4b2b062a2b24affe5fd262b
Nick Pierre <nickpierre95@gmail.com>
2017-10-25 16:10:47 -0400

    Update to use javaDocs in favor of block comments

--- src/test/java/lessonD_AdvancedStreams.java
+++ src/test/java/lessonD_AdvancedStreams.java
@@ -14,14 +14,14 @@ public class lessonD_AdvancedStreams {
     private String mEvenNums = "";
     private String mOddNums = "";
 
-    /*
-    So far everything has been pretty linear. Our pipelines all took the form:
-    "do this, then do this, then do this, then end". In reality we can combine pipelines. We can take two streams
-    and turn them into a single stream.
-
-    Now its worth nothing this is different from what we did when we nested Observables. In that case we always had one stream.
-    Lets take a stream of integers and a stream of strings and join them.
-    */
+    /**
+     * So far everything has been pretty linear. Our pipelines all took the form:
+     * "do this, then do this, then do this, then end". In reality we can combine pipelines. We can take two streams
+     * and turn them into a single stream.
+     *
+     * Now its worth nothing this is different from what we did when we nested Observables. In that case we always had one stream.
+     * Lets take a stream of integers and a stream of strings and join them.
+     */
     @Test
     public void _1_merging() {
         Observable<Object> you = Observable.just(1, 2, 3);
@@ -32,15 +32,15 @@ public class lessonD_AdvancedStreams {
         assertThat(mReceived).isEqualTo(_____);
     }
 
-    /*
-    We can also split up a single stream into two streams. We are going to to use the groupBy() action.
-    This action can be a little tricky because it emits an observable of observables. So we need to subscribe to the
-    "parent" observable and each emitted observable.
-
-    We encourage you to read more from the wiki: http://reactivex.io/documentation/operators/groupby.html
-
-    Lets split up a single stream of integers into two streams: even and odd numbers.
-    */
+    /**
+     * We can also split up a single stream into two streams. We are going to to use the groupBy() action.
+     * This action can be a little tricky because it emits an observable of observables. So we need to subscribe to the
+     * "parent" observable and each emitted observable.
+     *
+     * We encourage you to read more from the wiki: http://reactivex.io/documentation/operators/groupby.html
+     *
+     * Lets split up a single stream of integers into two streams: even and odd numbers.
+     */
     @Test
     public void _2_splittingUp() {
         Observable.range(1, 9)
@@ -62,12 +62,12 @@ public class lessonD_AdvancedStreams {
     }
 
 
-    /*
-    Lets take what we know now and do some cool stuff. We've setup an observable and a function for you. Lets combine
-    them together to average some numbers.
-
-    Also see that we need to subscribe first to the "parent" observable but that the pipeline still cold until we
-    subscribe to each subset observable. Don't forget to do that.
+    /**
+     * Lets take what we know now and do some cool stuff. We've setup an observable and a function for you. Lets combine
+     * them together to average some numbers.
+     *
+     * Also see that we need to subscribe first to the "parent" observable but that the pipeline still cold until we
+     * subscribe to each subset observable. Don't forget to do that.
      */
     @Test
     public void _3_challenge_needToSubscribeImmediatelyWhenSplitting() {
